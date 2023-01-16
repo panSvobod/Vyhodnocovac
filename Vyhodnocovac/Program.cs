4d33e9010c40ad03476c4616812aad8afcbef0e5
panSvobod <Svoboda.kr.2020@skola.ssps.cz>
2023-01-16 14:14:10 +0100

    Přidejte soubory projektu.

---
+++ Vyhodnocovac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vyhodnocovac
+{
+    public class Vyhodnotit
+    {
+        public double vyhodnotit(string a)
+        {
+            DataTable dt = new DataTable();
+            return Convert.ToDouble(dt.Compute(a, "")); ;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "2-1*8+16";
+            Vyhodnotit A = new Vyhodnotit();
+            Console.WriteLine(A.vyhodnotit(a));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vyhodnocovac/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Vyhodnocovac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vyhodnocovac")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("858506c8-6c1d-4cc6-a342-28b31e482c3d")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
